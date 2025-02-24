1dc5be88b74ac99fa8e7834ebf206ea8693f7ec7
Osmane <osmane.sgb@gmail.com>
2025-02-24 02:12:41 +0100

    ex00 c++ version

--- ex00/src/megaphone.cpp
+++ ex00/src/megaphone.cpp
@@ -1,31 +1,19 @@
 #include <iostream>
 
-// use to upper? simplify it?
 int	main(int ac, char **av)
 {
-	int i;
-	int j;
-
-	i = 1;
-
 	if (ac == 1)
 		std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *" << std::endl;
 	else
 	{
-		while (av[i])
+		for (int i = 1; av[i]; i++)
 		{
-			j = 0;
-			while (av[i][j])
-			{
-				if (av[i][j] >= 'a' && av[i][j] <= 'z')
-					av[i][j] -= 32;
-				j++;
-			}
+			for (int j = 0; av[i][j]; j++)
+				av[i][j] = toupper(av[i][j]);
 			if (av[i + 1])
 				std::cout << av[i] << " ";
 			else
 				std::cout << av[i] << std::endl;
-			i++;
 		}
 	}
 	return (0);
